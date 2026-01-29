c692b4242f74fa463ba9f5f8d4e4a950d25bb89b
unknown <opedrosantos16@gmail.com>
2026-01-29 18:55:04 -0300

    Tabela de Frequência

---
+++ freq_categoria.sql
@@ -0,0 +1,24 @@
+WITH tb_freq_abs AS (
+
+    SELECT descCategoriaProduto,
+            count(idTransacao) AS FreqAbs
+
+    FROM points
+    GROUP BY descCategoriaProduto
+
+),
+
+tb_freq_abs_cum AS (
+
+    SELECT *,
+        sum(FreqAbs) OVER (ORDER BY descCategoriaProduto) AS FreqAbsCum,
+        1.0 * FreqAbs / (SELECT sum(FreqAbs) FROM tb_freq_Abs) AS FreqRelativa
+
+    FROM tb_freq_abs
+
+)
+
+SELECT *,
+    sum(FreqRelativa) OVER (ORDER BY descCategoriaProduto) AS FreqRelativaCum
+
+FROM tb_freq_abs_cum 
